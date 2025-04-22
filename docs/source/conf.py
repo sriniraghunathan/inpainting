bde12f8e3dab2a532eb20af5595bd182a8ed8d8d
sriniraghuna <sriniraghuna@gmail.com>
2025-04-22 01:29:09 -0500

    more docs stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@ extensions = [
     'sphinx.ext.autodoc',        # To generate autodocs
     'sphinx.ext.mathjax',           # autodoc with maths
     'sphinx.ext.napoleon'           # For auto-doc configuration
-    'sphinx.ext.graphviz',
 ]
 
 napoleon_google_docstring = False   # Turn off googledoc strings
