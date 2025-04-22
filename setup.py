f78abefefb6e1379a0a244bb3b0cbd2538500f0f
sriniraghuna <sriniraghuna@gmail.com>
2025-04-22 12:31:09 -0500

    docs and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='inpainting',
+     author='Srini Raghunathan',
+      author_email='sriniraghuna@gmail.com',
+      zip_safe=False,
+      install_requires=[
+          'numpy',
+          # 'Sphinx',
+          ],
+      )
+
